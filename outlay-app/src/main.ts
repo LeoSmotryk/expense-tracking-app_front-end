f9b2a374bcf6fefbd4af7673a815564c13eab16d
Leonid Smotrykovskyi <kn1b22.smotrykovskyi@kpnu.edu.ua>
2025-01-10 02:25:39 +0200

    Change

--- outlay-app/src/main.ts
+++ outlay-app/src/main.ts
@@ -4,10 +4,9 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));*/
-  import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-  import { AppModule } from './app/app.module';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
   
-  platformBrowserDynamic()
-    .bootstrapModule(AppModule)
-    .catch((err) => console.error(err));
-  
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
